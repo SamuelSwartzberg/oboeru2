344dc2a2cd11bfa583cbc461385a772c0dc4e604
Sam Swartzberg <me@samswartzberg.com>
2022-02-22 06:50:13 +0100

    create logic for inserting sketchpad

--- src/ts/post-build/sketchpad/index.ts
+++ src/ts/post-build/sketchpad/index.ts
@@ -1,3 +1,23 @@
+import { addSketchpad } from "./sketchpad-builder";
+
+function isKanji(ch: string): boolean {
+  return (
+    (ch >= "\u4e00" && ch <= "\u9faf") || (ch >= "\u3400" && ch <= "\u4dbf")
+  );
+} // from https://www.darrenlester.com/blog/recognising-japanese-characters-with-javascript
+
 export function addSketchpadIfClozesWithKanji() {
-  throw new Error("Function not implemented.");
+  let activeClozes = document.querySelectorAll(".cloze-active");
+  let clozesWithKanji = Array.from(activeClozes).filter((cloze) => {
+    if (cloze.textContent) {
+      return Array.from(cloze.textContent).some(isKanji);
+    } else return false;
+  });
+  clozesWithKanji.forEach((cloze) => {
+    let nearestSection = cloze.closest(".section");
+    if (nearestSection) {
+      addSketchpad(nearestSection);
+    } else
+      throw new Error("Cloze with kanji is not in a section, which is odd.");
+  });
 }
---
+++ src/ts/post-build/sketchpad/sketchpad-builder/index.ts
@@ -0,0 +1,3 @@
+export function addSketchpad(nearestSection: Element) {
+  throw new Error("Function not implemented.");
+}
