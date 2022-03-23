c614704089b496e7bdf55bfb1222fe55323c7f26
Sam Swartzberg <me@samswartzberg.com>
2022-03-24 00:29:52 +0100

    continue tool work

---
+++ utilities/add-numbers-to-first-elem-in-line.ts
@@ -0,0 +1,26 @@
+import { annotateNumber } from "../card/ts/parsing/clozelike";
+import fs from "fs";
+
+const stdin = fs.readFileSync(process.stdin.fd).toString();
+const annotatedStdin = annotateNumber(stdin);
+
+let fusedStdin = [];
+let [stdinLines, annotatedStdinLines] = [
+  stdin.split("\n"),
+  annotatedStdin.split("\n"),
+];
+
+for (let index = 0; index < stdinLines.length; index++) {
+  let [stdinParts, annotatedStdinParts] = [
+    stdinLines[index].split("⟮"),
+    annotatedStdinLines[index].split(";"),
+  ];
+  let fusedLine = "";
+  if (stdinParts.length === 1) fusedLine = stdinParts[0];
+  else {
+    fusedLine = annotatedStdinParts[0] + ";" + stdinParts.slice(1).join("⟮");
+  }
+  fusedStdin.push(fusedLine);
+}
+
+console.log(fusedStdin.join("\n"));
