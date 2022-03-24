0097bd331b772967f7e2e5b7c89cf12688060c3f
Sam Swartzberg <me@samswartzberg.com>
2022-03-24 19:05:23 +0100

    continue working on implementation of three-step tree

--- card/ts/parsing/clozelike/3-parsed-tree-to-html/index.ts
+++ card/ts/parsing/clozelike/3-parsed-tree-to-html/index.ts
@@ -1,14 +1,13 @@
-import {
-  annnotateWithCountedNumber,
-  processCustomClozelikesToString,
-} from "../single-clozelike";
+
+import { annnotateWithCountedNumber } from "./reducers/number-only";
+import { getHTMLFromClozelike } from "./reducers/to-html";
 import { possiblyRecursiveTArray<string> } from "../1-string-to-tree";
 
 export function reduceTreeToHTML(
   clozeLikeArrayTree: possiblyRecursiveTArray<string>,
   isTop: boolean
 ): string {
-  return reduceTree(clozeLikeArrayTree, isTop, processCustomClozelikesToString);
+  return reduceTree(clozeLikeArrayTree, isTop, getHTMLFromClozelike);
 }
 
 export function annotateClozesInStructureWithCountedNumber(
@@ -20,11 +19,3 @@ export function annotateClozesInStructureWithCountedNumber(
 
 
 
-let activityClasses =
-    getClassesCorrespondingToCurrentMeaningOfClozelikeSpecifiers(
-      specifiers
-    ).join(" ");
-  return clozelikeHTMLFromStringConstituents(
-    contentAndHint.content,
-    activityClasses,
-    getHintStyleDeclarationIfAny(contentAndHint.hint)
