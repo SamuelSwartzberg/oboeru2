a5655b59a3cefdea409c21e909003145fff0d3f9
Sam Swartzberg <me@samswartzberg.com>
2022-04-11 15:28:46 +0200

    minor improvements

--- card/ts/parsing/container/stringify/tree-to-string.ts
+++ card/ts/parsing/container/stringify/tree-to-string.ts
@@ -23,12 +23,16 @@ export function treeToString(parsedSections: TreeNode<string>): string {
 }
 
 function buildHeader(title: string, isGroupShowHeader: boolean): string {
-  return `<h2 class="${getClassGroupShow(isGroupShowHeader)}">${title}</h2>`;
+  return `<summary class="${getClassGroupShow(
+    isGroupShowHeader
+  )}"><h2>${title}</h2></summary>`;
 }
 
 function buildArticle(header: string, content: string, depth: number): string {
-  return `<article class="indent-${depth - 1} headered-container cloze-group">
+  return `<details open class="indent-${
+    depth - 1
+  } headered-container cloze-group">
     ${header}
     ${content}
-  </article>`;
+  </details>`;
 }
