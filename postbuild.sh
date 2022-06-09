3b2e6182292b9b09c3c05fbf04e812f5bfe0627e
Sam Swartzberg <me@samswartzberg.com>
2022-06-10 01:10:40 +0200

    move postbuild step into own script

---
+++ postbuild.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+[[ -z "$ANKI_COLLECTION_MEDIA" ]] && exit 1; 
+cp dist/card/anki_bundle.js dist/card/_bundle.css "$ANKI_COLLECTION_MEDIA";
+[[ -z "$MFLASHCARDS" ]] && exit 1;
+cp dist/card/web_bundle.js dist/card/_bundle.css "$MFLASHCARDS";
