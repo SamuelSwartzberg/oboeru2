585b4d38ac709130178b312ef96e8ead9e6a725e
Sam Swartzberg <me@samswartzberg.com>
2022-07-20 16:14:43 +0200

    add more fancy table formatting

--- card/ts/post-build/index.ts
+++ card/ts/post-build/index.ts
@@ -6,4 +6,12 @@ export function decoratePostBuild(templateTree: DocumentFragment): void {
   formatVisibleClozeGroups(templateTree);
   highlightVisibleCode(templateTree);
   addSketchpadIfClozesWithKanji(templateTree);
+  addAdditionalClasses(templateTree);
+}
+function addAdditionalClasses(templateTree: DocumentFragment) {
+  templateTree
+    .querySelectorAll(".section > .section-inner-table")
+    .forEach((sectionInnerTable: Element) => {
+      sectionInnerTable.parentElement?.classList.add("contains-table");
+    });
 }
